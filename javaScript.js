8821c9d9cf607935fbeb33ab139a9070f43d14ca
B E <202210@NTTDATA.COM>
2020-03-12 11:33:54 +0530

    JavaScript basics commit

--- javaScript.js
+++ javaScript.js
@@ -1,2 +1,50 @@
-var firstName  = 'Kiran';
+
+/***********************
+ * JavaScipts Basics
+ ***********************/
+
+//variables
+var firstName = 'Kiran';
+
+//Data Types in JavaScript : JavaScript has dynamic typing ( Data types are automatically assigned to variables)
+
+/**
+ * Number     : Floating point numbers, Decimals and integers
+ * String     : Sequence of characters
+ * Boolean    : Logical data types tat can be true or false
+ * undefined  : Data type of a variable that dose not hold any value
+ * null       : Non-existent
+ **/
+
+var firstName = 'Kiran'; //String
 console.log(firstName);
+
+var lastName = 'be';
+console.log(lastName);
+
+var birthDate = 02; //Number
+console.log(birthDate);
+
+var job; //Undefined
+console.log(job);
+
+var job = 'Developer';
+console.log(job)
+
+/**
+ * Variable mutation and type coercion
+ */
+
+ var month = 'March';
+ var year = 2020;
+ console.log(month + ' ' + year); 
+ //type coercion: JS automatically converts one datatype to another - year (number) is converted to string.
+ // 3 string - month, space and year are displayed.
+
+ 
+
+
+
+
+
+
